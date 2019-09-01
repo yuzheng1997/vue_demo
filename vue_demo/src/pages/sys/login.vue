22aedeb2393b24708e54e61f740c36e3083c1469
yuzheng <925685999@qq.com>
2019-09-01 08:57:51 +0800

    vue绑定style小小demo

--- vue_demo/src/pages/sys/login.vue
+++ vue_demo/src/pages/sys/login.vue
@@ -1,22 +1,54 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="login">
+    <el-form>
+      <el-row style="text-align: center">
+        <h3>登录</h3>
+      </el-row>
+      <el-row>
+            <el-form-item>
+              <el-input placeholder="用户名"></el-input>
+            </el-form-item>
+      </el-row>
+      <el-row>
+          <el-form-item>
+            <el-input show-password placeholder="密码"></el-input>
+          </el-form-item>
+      </el-row>
+      <el-row>
+          <el-form-item>
+            <el-button type="primary">登录</el-button>
+          </el-form-item>
+      </el-row>
+    </el-form>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'App'
+  name: 'login'
 }
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  #login {
+    border-radius: 2%;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    background-color: #FFFFCC;
+    border: azure 1px;
+    position: absolute;
+    width: 400px;
+    height: 500px;
+    margin: -250px 0 0 -200px ;
+    left: 50%;
+    top: 50%;
+  }
+  .el-button {
+    width: 300px;
+    background-color: #CCFFFF;
+  }
+  .el-button:hover {
+    background-color: #99CCFF;
+  }
+  .el-input {
+    width: 300px;
   }
 </style>
